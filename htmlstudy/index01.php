2190e13b6f84ff4e129258b699532ee306c85095
munishstudy73 <72012983+munishstudy73@users.noreply.github.com>
2020-10-13 17:27:38 +0530

    Create index01.php

---
+++ htmlstudy/index01.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+<title> My First Web Page</title>
+<meta name="description" content="my first page">
+<meta tag="keywords" conetent="html study">
+<head>
+
+</head>
+<body>
+	
+This is my first page.
+Learn about headers
+<h1> This is level 1 header</h1>
+<h2>This is level 2 header</h2>
+
+<p> This is my first paragraph </p>
+<p> this is my second para. 111111111111111111111111111111111111111
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaa </p>
+<p> bold tag for next word <b> bold </b> </p>
+<p> italic tag for next word <i> italic </i></p>
+<p> underline tag for bet word <u> underline </u></p>
+<p> superscript tag for next word <sup> superscript</sup><p>
+<p> subscript tag for next word <sub> subscript </sub></p>
+ <p> next line is horizontal line</p>
+ <hr>
+ <p> This is nesting of tag with <b>bold <i>and</i> itaic</b> </p> 	
+ <p> Next we learn about special characters. We can have special character like &copy and &lt</p>
+
+<pre> If we want to pre format
+   line like this one we 
+need pre format tag</pre>
+
+<p>comment is simple use of tag </p>
+<!--  comment tag          -->
+<p>Next study about link tag </p>
+<a href="http://www.google.com"> google link</a>
+
+
+
+
+
+
+
+</body>
+</html>
