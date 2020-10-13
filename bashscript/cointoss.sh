3fd9ea737fe161dc23af238221faf51df795045b
munishstudy73 <munishstudy73@gmail.com>
2020-10-13 04:47:52 -0700

    commit

---
+++ bashscript/cointoss.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+#Author
+#Date
+#Program - Coin toss program
+
+#!/bin/bash
+Result=$((RANDOM%2))
+if [[ ${Result} -eq 0 ]]; then
+    echo HEADS
+elif [[ ${Result} -eq 1 ]]; then
+    echo TAILS
+fi
